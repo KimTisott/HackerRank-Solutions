056c4191ea298d8ad60ff46579a7a11d6c20d67e
KimTisott <40810123+KimTisott@users.noreply.github.com>
2021-03-09 11:10:35 -0300

    Create Binary Tree Nodes.sql

---
+++ SQL/Binary Tree Nodes.sql
@@ -0,0 +1,10 @@
+SELECT n, CASE WHEN p IS NULL THEN 'Root'
+               WHEN EXISTS(SELECT p
+                             FROM bst
+                            WHERE b.n = p
+                          ) THEN 'Inner'
+               ELSE 'Leaf'
+           END
+  FROM bst AS b
+ ORDER BY n
+;
