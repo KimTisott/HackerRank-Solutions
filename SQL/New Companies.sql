e807f9a4508e11a67dd4df786e9698c4a4ec60a1
KimTisott <40810123+KimTisott@users.noreply.github.com>
2021-03-09 11:44:58 -0300

    Create New Companies.sql

---
+++ SQL/New Companies.sql
@@ -0,0 +1,17 @@
+SELECT c.company_code, c.founder
+       , COUNT(DISTINCT l.lead_manager_code)
+       , COUNT(DISTINCT s.senior_manager_code)
+       , COUNT(DISTINCT m.manager_code)
+       , COUNT(DISTINCT e.employee_code)
+  FROM company AS c, 
+       lead_manager AS l, 
+       senior_manager AS s, 
+       manager AS m, 
+       employee AS e
+ WHERE c.company_code = l.company_code
+   AND l.lead_manager_code = s.lead_manager_code
+   AND s.senior_manager_code = m.senior_manager_code
+   AND m.manager_code = e.manager_code
+ GROUP BY c.company_code, c.founder
+ ORDER BY company_code
+;
