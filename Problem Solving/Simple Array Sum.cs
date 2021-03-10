7a6cd5706d2158aef86ae6d0537117ed42c5d818
KimTisott <40810123+KimTisott@users.noreply.github.com>
2021-03-10 17:27:48 -0300

    Update Simple Array Sum.cs

--- Problem Solving/Simple Array Sum.cs
+++ Problem Solving/Simple Array Sum.cs
@@ -1,18 +1,6 @@
-using System;
-using System.Collections.Generic;
-using System.IO;
 using System.Linq;
 class Solution {
-    static int simpleArraySum(int[] ar) {
+    int simpleArraySum(int[] ar) {
         return ar.Sum();
-    }
-    static void Main(string[] args) {
-        int arCount = Convert.ToInt32(Console.ReadLine());
-        int[] ar = Array.ConvertAll(Console.ReadLine().Split(' '), arTemp => Convert.ToInt32(arTemp));
-        int result = simpleArraySum(ar);
-        TextWriter textWriter = new StreamWriter(@System.Environment.GetEnvironmentVariable("OUTPUT_PATH"), true);
-        textWriter.WriteLine(result);
-        textWriter.Flush();
-        textWriter.Close();
     }
 }
