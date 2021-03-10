e365bf6d75a6a979e9fa1070cb5823b6a2b10bd1
KimTisott <40810123+KimTisott@users.noreply.github.com>
2021-03-10 09:26:08 -0300

    Create Symmetric Pairs.sql

---
+++ SQL/Symmetric Pairs.sql
@@ -0,0 +1,8 @@
+SELECT f1.x, f1.y
+  FROM functions AS f1
+       JOIN functions AS f2
+       ON f1.x = f2.y AND f1.y = f2.x
+ GROUP BY f1.x, f1.y
+ HAVING COUNT(f1.x) > 1 OR f1.x < f1.y
+ ORDER BY f1.x
+;
