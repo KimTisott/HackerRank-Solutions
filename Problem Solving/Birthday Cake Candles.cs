b2e85dd6f10f05e61fba9671d8f94a789889a0c8
KimTisott <40810123+KimTisott@users.noreply.github.com>
2021-03-10 17:21:52 -0300

    Update Birthday Cake Candles.cs

--- Problem Solving/Birthday Cake Candles.cs
+++ Problem Solving/Birthday Cake Candles.cs
@@ -1,50 +1,8 @@
-using System.CodeDom.Compiler;
 using System.Collections.Generic;
-using System.Collections;
-using System.ComponentModel;
-using System.Diagnostics.CodeAnalysis;
-using System.Globalization;
-using System.IO;
 using System.Linq;
-using System.Reflection;
-using System.Runtime.Serialization;
-using System.Text.RegularExpressions;
-using System.Text;
-using System;
-
-class Result
-{
-
-    /*
-     * Complete the 'birthdayCakeCandles' function below.
-     *
-     * The function is expected to return an INTEGER.
-     * The function accepts INTEGER_ARRAY candles as parameter.
-     */
-
-    public static int birthdayCakeCandles(List<int> candles)
-    {
-        int max = candles.Max();
+class Solution {
+    int birthdayCakeCandles(List<int> candles) {
+        var max = candles.Max();
         return candles.Count(x => x == max);
-    }
-
-}
-
-class Solution
-{
-    public static void Main(string[] args)
-    {
-        TextWriter textWriter = new StreamWriter(@System.Environment.GetEnvironmentVariable("OUTPUT_PATH"), true);
-
-        int candlesCount = Convert.ToInt32(Console.ReadLine().Trim());
-
-        List<int> candles = Console.ReadLine().TrimEnd().Split(' ').ToList().Select(candlesTemp => Convert.ToInt32(candlesTemp)).ToList();
-
-        int result = Result.birthdayCakeCandles(candles);
-
-        textWriter.WriteLine(result);
-
-        textWriter.Flush();
-        textWriter.Close();
     }
 }
