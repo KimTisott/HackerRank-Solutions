8bd2bc4c1489374319878e42863d1371f5a879c3
KimTisott <40810123+KimTisott@users.noreply.github.com>
2021-03-10 09:05:28 -0300

    Create Placements.sql

---
+++ SQL/Placements.sql
@@ -0,0 +1,11 @@
+SELECT s.name
+  FROM students AS s
+       INNER JOIN packages AS p1
+       ON s.id = p1.id
+       INNER JOIN friends AS f
+       ON s.id = f.id
+       INNER JOIN packages AS p2
+       ON f.friend_id = p2.id
+ WHERE p2.salary > p1.salary
+ ORDER BY p2.salary
+;
