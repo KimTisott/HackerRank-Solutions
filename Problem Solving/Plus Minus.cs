86886534fa751f4796647784df0117003d66c206
KimTisott <40810123+KimTisott@users.noreply.github.com>
2021-03-10 17:27:15 -0300

    Update Plus Minus.cs

--- Problem Solving/Plus Minus.cs
+++ Problem Solving/Plus Minus.cs
@@ -1,32 +1,10 @@
-using System.CodeDom.Compiler;
-using System.Collections.Generic;
-using System.Collections;
-using System.ComponentModel;
-using System.Diagnostics.CodeAnalysis;
-using System.Globalization;
-using System.IO;
-using System.Linq;
-using System.Reflection;
-using System.Runtime.Serialization;
-using System.Text.RegularExpressions;
-using System.Text;
 using System;
-
+using System.Linq;
 class Solution {
-
-    // Complete the plusMinus function below.
-    static void plusMinus(int[] arr) {
+    void plusMinus(int[] arr) {
         var total = arr.Count();
         Console.WriteLine(((double)arr.Count(x => x > 0) / total).ToString("N6"));
         Console.WriteLine(((double)arr.Count(x => x < 0) / total).ToString("N6"));
         Console.WriteLine(((double)arr.Count(x => x == 0) / total).ToString("N6"));
-    }
-
-    static void Main(string[] args) {
-        int n = Convert.ToInt32(Console.ReadLine());
-
-        int[] arr = Array.ConvertAll(Console.ReadLine().Split(' '), arrTemp => Convert.ToInt32(arrTemp))
-        ;
-        plusMinus(arr);
     }
 }
