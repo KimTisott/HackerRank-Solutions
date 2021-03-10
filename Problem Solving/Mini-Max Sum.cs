d9f413dbffedd6d17ca4daa85f60f15bea0133da
KimTisott <40810123+KimTisott@users.noreply.github.com>
2021-03-10 17:26:21 -0300

    Update Mini-Max Sum.cs

--- Problem Solving/Mini-Max Sum.cs
+++ Problem Solving/Mini-Max Sum.cs
@@ -1,27 +1,8 @@
-using System.CodeDom.Compiler;
-using System.Collections.Generic;
-using System.Collections;
-using System.ComponentModel;
-using System.Diagnostics.CodeAnalysis;
-using System.Globalization;
-using System.IO;
-using System.Linq;
-using System.Reflection;
-using System.Runtime.Serialization;
-using System.Text.RegularExpressions;
-using System.Text;
 using System;
-
+using System.Linq;
 class Solution {
-
-    // Complete the miniMaxSum function below.
-    static void miniMaxSum(int[] arr) {
-        var ordered = arr.OrderBy(x => x);
-        Console.WriteLine($"{ordered.Take(4).Sum(x => Convert.ToInt64(x))} {(long)ordered.Skip(1).Take(4).Sum(x => Convert.ToInt64(x))}");
-    }
-
-    static void Main(string[] args) {
-        int[] arr = Array.ConvertAll(Console.ReadLine().Split(' '), arrTemp => Convert.ToInt32(arrTemp));
-        miniMaxSum(arr);
+    void miniMaxSum(int[] arr) {
+        var orderedArr = arr.OrderBy(x => x);
+        Console.WriteLine($"{orderedArr.Take(4).Sum(x => Convert.ToInt64(x))} {(long)orderedArr.Skip(1).Take(4).Sum(x => Convert.ToInt64(x))}");
     }
 }
