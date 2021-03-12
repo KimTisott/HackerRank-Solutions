363cea0bd940c646ca90f3e5a24e8f42d23fff79
KimTisott <40810123+KimTisott@users.noreply.github.com>
2021-03-12 08:47:52 -0300

    Create Apple and Orange.cs

---
+++ Problem Solving/Apple and Orange.cs
@@ -0,0 +1,8 @@
+using System;
+using System.Linq;
+class Solution {
+    void countApplesAndOranges(int s, int t, int a, int b, int[] apples, int[] oranges) {
+        Console.WriteLine(apples.Count(x => x + a >= s && x + a <= t));
+        Console.WriteLine(oranges.Count(x => x + b >= s && x + b <= t));
+    }
+}
