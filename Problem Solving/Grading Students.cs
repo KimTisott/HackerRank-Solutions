95de26a4a7877d324f8123c6909e67ef1ae5edcf
KimTisott <40810123+KimTisott@users.noreply.github.com>
2021-03-12 08:46:45 -0300

    Create Grading Students.cs

---
+++ Problem Solving/Grading Students.cs
@@ -0,0 +1,8 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+class Solution {
+    List<int> gradingStudents(List<int> grades) {
+        return grades.Select(x => x >= 38 && x % 5 > 2 ? (int)Math.Round(x / 5.0) * 5 : x).ToList();
+    }
+}
