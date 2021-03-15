7a73d51f4b196ad0210cf2b2b663e6582f98603e
KimTisott <40810123+KimTisott@users.noreply.github.com>
2021-03-15 08:41:16 -0300

    Update and rename SQL/Type of Triangle.sql to SQL/Advanced Select/Type of Triangle.sql

--- SQL/Advanced Select/Type of Triangle.sql
+++ SQL/Advanced Select/Type of Triangle.sql
@@ -2,4 +2,3 @@ SELECT IF (a + b <= c OR a + c <= b OR b + c <= a, 'Not A Triangle',
        IF (a = b AND b = c, 'Equilateral',
        IF (a = b OR b = c OR a = c, 'Isosceles', 'Scalene')))
   FROM triangles
-;
