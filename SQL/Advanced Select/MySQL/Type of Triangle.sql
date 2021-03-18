3b4047e9a7e6fc289fdc39515e21060f2e9478e1
KimTisott <40810123+KimTisott@users.noreply.github.com>
2021-03-18 09:12:53 -0300

    Update Type of Triangle.sql

--- SQL/Advanced Select/MySQL/Type of Triangle.sql
+++ SQL/Advanced Select/MySQL/Type of Triangle.sql
@@ -1,4 +1,6 @@
-SELECT IF (a + b <= c OR a + c <= b OR b + c <= a, 'Not A Triangle',
-       IF (a = b AND b = c, 'Equilateral',
-       IF (a = b OR b = c OR a = c, 'Isosceles', 'Scalene')))
+SELECT CASE WHEN (a + b <= c OR a + c <= b OR b + c <= a) THEN 'Not A Triangle'
+            WHEN (a = b AND b = c) THEN 'Equilateral'
+            WHEN (a = b OR b = c OR a = c) THEN 'Isosceles'
+            ELSE 'Scalene'
+       END
   FROM triangles
