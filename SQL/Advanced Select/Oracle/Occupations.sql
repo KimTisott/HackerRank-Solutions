125350621f66fcdecfe944ea9de535c84676a5e5
KimTisott <40810123+KimTisott@users.noreply.github.com>
2021-03-18 09:48:51 -0300

    Create Occupations.sql

---
+++ SQL/Advanced Select/Oracle/Occupations.sql
@@ -0,0 +1,11 @@
+SELECT doctor, professor, singer, actor 
+  FROM (SELECT name, occupation, ROW_NUMBER() OVER(PARTITION BY occupation ORDER BY name) AS rownumber 
+          FROM occupations
+       )
+ PIVOT (MAX(name) FOR occupation IN('Doctor' AS doctor,
+                                    'Professor' AS professor,
+                                    'Singer' AS singer,
+                                    'Actor' AS actor
+                                   )
+       ) ORDER BY rownumber
+;
