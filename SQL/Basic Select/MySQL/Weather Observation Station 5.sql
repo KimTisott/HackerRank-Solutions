45e6836754be2d021b0686469de53a88a0fa191b
KimTisott <40810123+KimTisott@users.noreply.github.com>
2021-03-18 08:30:43 -0300

    Update Weather Observation Station 5.sql

--- SQL/Basic Select/MySQL/Weather Observation Station 5.sql
+++ SQL/Basic Select/MySQL/Weather Observation Station 5.sql
@@ -1,11 +1,13 @@
-SELECT * FROM (SELECT city, LENGTH(city) AS city_length
-                 FROM station
-                ORDER BY city_length, city
-                LIMIT 1
-              ) AS s
-                UNION
-SELECT * FROM (SELECT city, LENGTH(city) AS city_length
-                 FROM station
-                ORDER BY city_length DESC, city
-                LIMIT 1
-              ) AS l
+SELECT * 
+  FROM (SELECT city, LENGTH(city) AS city_length
+          FROM station
+         ORDER BY city_length, city
+         LIMIT 1
+       ) AS s
+ UNION
+SELECT * 
+  FROM (SELECT city, LENGTH(city) AS city_length
+          FROM station
+         ORDER BY city_length DESC, city
+         LIMIT 1
+       ) AS l
