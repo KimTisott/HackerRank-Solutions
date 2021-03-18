4be0b380e58d78b2b28f2ff1f6b17b61b4256df0
KimTisott <40810123+KimTisott@users.noreply.github.com>
2021-03-18 09:22:58 -0300

    Update Occupations.sql

--- SQL/Advanced Select/MSSQL/Occupations.sql
+++ SQL/Advanced Select/MSSQL/Occupations.sql
@@ -1,5 +1,5 @@
 SELECT doctor, professor, singer, actor
-  FROM (SELECT ROW_NUMBER() OVER (PARTITION BY OCCUPATION ORDER BY NAME) ROWNUMBER, 
+  FROM (SELECT ROW_NUMBER() OVER (PARTITION BY occupation ORDER BY name) ROWNUMBER, 
                * FROM occupations
        ) AS t
  PIVOT (MAX(name) FOR occupation IN (doctor, professor, singer, actor)
