c83ac043c5de478a758295574555d8dacc3a8a74
KimTisott <40810123+KimTisott@users.noreply.github.com>
2021-03-18 09:37:56 -0300

    Update The PADS.sql

--- SQL/Advanced Select/MSSQL/The PADS.sql
+++ SQL/Advanced Select/MSSQL/The PADS.sql
@@ -1,7 +1,7 @@
 SELECT CONCAT(name, '(', SUBSTRING(occupation, 1, 1), ')')
   FROM occupations
  ORDER BY name
-SELECT CONCAT("There are a total of ", COUNT(occupation), " ", LOWER(occupation), "s.") AS total
+SELECT CONCAT('There are a total of ', COUNT(occupation), ' ', LOWER(occupation), 's.') AS total
   FROM occupations
  GROUP BY occupation
  ORDER BY total
