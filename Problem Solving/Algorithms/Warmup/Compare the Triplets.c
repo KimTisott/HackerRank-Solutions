160aedbd370277be354cc45e8d1dd9a72702a5ee
KimTisott <40810123+KimTisott@users.noreply.github.com>
2021-06-16 20:35:21 -0300

    Create Compare the Triplets.c

---
+++ Problem Solving/Algorithms/Warmup/Compare the Triplets.c
@@ -0,0 +1,15 @@
+int* compareTriplets(int a_count, int* a, int b_count, int* b, int* result_count)
+{
+    *result_count = 2;
+    
+    int* result = calloc(*result_count, sizeof(int));
+    
+    for (int i = 0; i < a_count; i++)
+    {
+        result[0] += a[i] > b[i];
+        
+        result[1] += a[i] < b[i];
+    }
+    
+    return result;
+}
