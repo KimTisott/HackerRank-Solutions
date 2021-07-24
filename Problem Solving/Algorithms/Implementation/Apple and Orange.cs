0f1f058a86fb2aead3fceb7af912af555fea0c51
KimTisott <40810123+KimTisott@users.noreply.github.com>
2021-07-24 10:37:34 -0300

    Update Apple and Orange.cs

--- Problem Solving/Algorithms/Implementation/Apple and Orange.cs
+++ Problem Solving/Algorithms/Implementation/Apple and Orange.cs
@@ -1,8 +1,12 @@
 using System;
 using System.Linq;
-class Solution {
-    void countApplesAndOranges(int s, int t, int a, int b, int[] apples, int[] oranges) {
+
+class Solution
+{
+    void countApplesAndOranges(int s, int t, int a, int b, int[] apples, int[] oranges)
+    {
         Console.WriteLine(apples.Count(x => x + a >= s && x + a <= t));
+        
         Console.WriteLine(oranges.Count(x => x + b >= s && x + b <= t));
     }
 }
