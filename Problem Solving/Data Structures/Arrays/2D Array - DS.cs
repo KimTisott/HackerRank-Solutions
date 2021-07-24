63174b7f1571a2d99015fa2c728fab560f0ef5e0
KimTisott <40810123+KimTisott@users.noreply.github.com>
2021-07-24 10:41:44 -0300

    Update 2D Array - DS.cs

--- Problem Solving/Data Structures/Arrays/2D Array - DS.cs
+++ Problem Solving/Data Structures/Arrays/2D Array - DS.cs
@@ -1,7 +1,10 @@
 using System.Collections.Generic;
 using System.Linq;
-class Solution {
-    int hourglassSum(List<List<int>> arr) {
+
+class Solution
+{
+    int hourglassSum(List<List<int>> arr)
+    {
         return Enumerable.Range(0, 4).SelectMany(x => Enumerable.Range(0, 4).Select(y => arr[x][y] + arr[x][y+1] + arr[x][y+2] + arr[x+1][y+1] + arr[x+2][y] + arr[x+2][y+1] + arr[x+2][y+2])).Max();
     }
 }
