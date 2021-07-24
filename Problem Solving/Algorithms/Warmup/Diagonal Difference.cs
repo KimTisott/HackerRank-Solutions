ae8a3d420cec4bfb72ca1f9c321e8cdbf87f75f4
KimTisott <40810123+KimTisott@users.noreply.github.com>
2021-07-24 10:40:11 -0300

    Update Diagonal Difference.cs

--- Problem Solving/Algorithms/Warmup/Diagonal Difference.cs
+++ Problem Solving/Algorithms/Warmup/Diagonal Difference.cs
@@ -1,8 +1,11 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
-class Solution {
-    int diagonalDifference(List<List<int>> arr) {
+
+class Solution
+{
+    int diagonalDifference(List<List<int>> arr)
+    {
         return Math.Abs(arr.Sum(x => x[arr.IndexOf(x)]) - arr.Sum(x => x[arr.Count - arr.IndexOf(x) - 1]));
     }
 }
