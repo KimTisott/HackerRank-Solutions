dc0bc647b25dad304590f054e302a73590e650c7
KimTisott <40810123+KimTisott@users.noreply.github.com>
2021-07-24 10:40:36 -0300

    Update Plus Minus.cs

--- Problem Solving/Algorithms/Warmup/Plus Minus.cs
+++ Problem Solving/Algorithms/Warmup/Plus Minus.cs
@@ -1,10 +1,16 @@
 using System;
 using System.Linq;
-class Solution {
-    void plusMinus(int[] arr) {
+
+class Solution
+{
+    void plusMinus(int[] arr)
+    {
         var total = arr.Count();
+        
         Console.WriteLine(((double)arr.Count(x => x > 0) / total).ToString("N6"));
+        
         Console.WriteLine(((double)arr.Count(x => x < 0) / total).ToString("N6"));
+        
         Console.WriteLine(((double)arr.Count(x => x == 0) / total).ToString("N6"));
     }
 }
