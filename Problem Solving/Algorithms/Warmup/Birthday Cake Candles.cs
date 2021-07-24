667e88cd3f8e1c2c26cc559764073c31108a8860
KimTisott <40810123+KimTisott@users.noreply.github.com>
2021-07-24 10:39:40 -0300

    Update Birthday Cake Candles.cs

--- Problem Solving/Algorithms/Warmup/Birthday Cake Candles.cs
+++ Problem Solving/Algorithms/Warmup/Birthday Cake Candles.cs
@@ -1,8 +1,12 @@
 using System.Collections.Generic;
 using System.Linq;
-class Solution {
-    int birthdayCakeCandles(List<int> candles) {
+
+class Solution
+{
+    int birthdayCakeCandles(List<int> candles)
+    {
         var max = candles.Max();
+        
         return candles.Count(x => x == max);
     }
 }
