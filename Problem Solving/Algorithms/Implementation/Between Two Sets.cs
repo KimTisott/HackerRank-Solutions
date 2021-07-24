4dad61a4c4732381cd4b276a4a8e4098b39155a4
KimTisott <40810123+KimTisott@users.noreply.github.com>
2021-07-24 10:38:24 -0300

    Update Between Two Sets.cs

--- Problem Solving/Algorithms/Implementation/Between Two Sets.cs
+++ Problem Solving/Algorithms/Implementation/Between Two Sets.cs
@@ -1,27 +1,47 @@
 using System.Collections.Generic;
 using System.Linq;
-class Solution {
-    int getTotalX(List<int> a, List<int> b) {
+
+class Solution
+{
+    int getTotalX(List<int> a, List<int> b)
+    {
         int result = 0, counter = 1, aMax = a.Max(), bMin = b.Min(), aMaxMultiple = aMax;
-        while (aMaxMultiple <= bMin) {
+        
+        while (aMaxMultiple <= bMin)
+        {
             var isFactor = true;
+            
             foreach (var item in a)
-                if (aMaxMultiple % item != 0) {
+            {
+                if (aMaxMultiple % item != 0)
+                {
                     isFactor = false;
                     break;
                 }
-            if (isFactor) {
-                foreach (var item in b) 
-                    if (item % aMaxMultiple != 0) {
+            }
+            
+            if (isFactor)
+            {
+                foreach (var item in b)
+                {
+                    if (item % aMaxMultiple != 0)
+                    {
                         isFactor = false;
                         break;
                     }
+                }
             }
+            
             if (isFactor)
+            {
                 result++;
+            }
+            
             counter++;
+            
             aMaxMultiple = aMax * counter;
         }
+        
         return result;
     }
 }
