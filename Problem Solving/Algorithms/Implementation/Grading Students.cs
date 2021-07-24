af75a748078a38e56656a4164aa56bddb84cd005
KimTisott <40810123+KimTisott@users.noreply.github.com>
2021-07-24 10:38:55 -0300

    Update Grading Students.cs

--- Problem Solving/Algorithms/Implementation/Grading Students.cs
+++ Problem Solving/Algorithms/Implementation/Grading Students.cs
@@ -1,8 +1,11 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
-class Solution {
-    List<int> gradingStudents(List<int> grades) {
+
+class Solution
+{
+    List<int> gradingStudents(List<int> grades)
+    {
         return grades.Select(x => x >= 38 && x % 5 > 2 ? (int)Math.Round(x / 5.0) * 5 : x).ToList();
     }
 }
