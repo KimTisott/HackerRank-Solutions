edb6181bfd2745b9ce30f09e2fd7a834a2a3e759
KimTisott <40810123+KimTisott@users.noreply.github.com>
2021-07-24 10:39:58 -0300

    Update Compare the Triplets.cs

--- Problem Solving/Algorithms/Warmup/Compare the Triplets.cs
+++ Problem Solving/Algorithms/Warmup/Compare the Triplets.cs
@@ -1,10 +1,14 @@
 using System.Collections.Generic;
 using System.Linq;
-class Solution {
-    List<int> compareTriplets(List<int> a, List<int> b) {
-        return new List<int>() {
-            a.Count(x => x > b[a.IndexOf(x)])
-          , b.Count(x => x > a[b.IndexOf(x)])
+
+class Solution
+{
+    List<int> compareTriplets(List<int> a, List<int> b)
+    {
+        return new List<int>()
+        {
+            a.Count(x => x > b[a.IndexOf(x)]),
+            b.Count(x => x > a[b.IndexOf(x)])
         };
     }
 }
