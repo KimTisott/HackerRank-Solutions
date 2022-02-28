f96e2773df0dfc1f187706e8e878b715991c29e0
Kennethcodes <39102148+Snakeybob@users.noreply.github.com>
2022-02-28 23:56:10 +0800

    Update Print Prime Numbers.sql
    Added comments and explanation of how the code works

--- SQL/Alternative Queries/Print Prime Numbers.sql
+++ SQL/Alternative Queries/Print Prime Numbers.sql
@@ -11,3 +11,45 @@ SELECT GROUP_CONCAT(n SEPARATOR '&')
                            LIMIT 1000
                          ) AS n2
                    WHERE MOD(n, d) = 0 AND n <> d)
+
+
+/*
+Output:
+    All prime numbers
+
+Condition:
+    1. Prime numbers that are less than or equal to 1000
+    2. Print result on a single line
+    3. Use ampersand (&) as separator instead of a space
+    
+Note:
+    Prime numbers properties: Any whole number greater than 1 that is divisible only by 1 and itself, is defined as a prime number
+*/
+
+-- This concatenate the output with '&' as separator
+SELECT GROUP_CONCAT(n SEPARATOR '&')
+-- Two information_schema.tables were created because it has to store 1000 rows and one table is not enough (someone online says it only contains 63 rows per table), so two tables together can store 63 * 63 rows which is > 1000
+FROM 
+    (
+    SELECT @num := @num+1 as n 
+    FROM
+        information_schema.tables t1,
+        information_schema.tables t2,
+        (SELECT @num := 1) tmp
+    ) tempNum
+-- This limits the output to be less than or equal to 1000 as required
+WHERE n <= 1000 
+-- This part is to make sure the output is not in the set of non-prime number, hence prime number. For n in the set where n can be divided by d with a remainder of 0 and n is not d.
+AND NOT EXISTS
+    (
+    SELECT * 
+    FROM 
+        (SELECT @nu := @nu+1 as d 
+         FROM
+            information_schema.tables t1,
+            information_schema.tables t2,
+            (SELECT @nu := 1) tmp1
+            LIMIT 1000
+        ) tatata
+    WHERE n % d = 0 AND n <> d
+    )
