c5aea462ed6459816618a5840aad508d5764564c
KimTisott <40810123+KimTisott@users.noreply.github.com>
2022-03-01 20:58:41 -0300

    Added ProjectEuler+/001

---
+++ Contests/ProjectEuler+/Multiples of 3 and 5.cs
@@ -0,0 +1,27 @@
+using System;
+
+class Solution
+{
+
+    static void Main(String[] args)
+    {
+        int t = Convert.ToInt32(Console.ReadLine());
+        for (int a0 = 0; a0 < t; a0++)
+        {
+            int n = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine(Euler001((ulong)n));
+        }
+    }
+
+    static ulong Euler001(ulong n)
+    {
+        var limit = n - 1;
+        return TotalSum(limit, 3) + TotalSum(limit, 5) - TotalSum(limit, 15);
+    }
+
+    static ulong TotalSum(ulong limit, ulong multiple)
+    {
+        var count = limit / multiple;
+        return multiple * count * (count + 1) >> 1;
+    }
+}
